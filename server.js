328a1e3eeafc9355a78a39fce295cb22bb30305e
Bentleybb <liontuzi@gmail.com>
2025-12-06 02:49:05 -0500

    Added profile and edit profile pages with new functionality

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
+const profileRoutes = require('./src/routes/profile')(pool, verifyToken);
+app.use('/api/profile', profileRoutes);
+
 // MySQL connection pool
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
